9b22df50d3b7fdb3919c203864b55689f24233cb
Alexey <theivlev@yandex.ru>
2023-01-24 22:16:18 +0400

    Авторизация и регистрация 3

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render
+from random import randint
+# Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import action, api_view, permission_classes
+from django.core.mail import send_mail
+from reviews.models import Category, Genre, Title,  User
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer,
+    ReviewSerializer,
+    SignUpSerializer,
+    UserSerializer,
+    TokenSerializer
+)
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens  import  RefreshToken
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # permission_classes =
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    # permission_classes =
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = (Title.objects.all()
+                .select_related('category')
+                .prefetch_related('genre'))
+    serializer_class = TitleSerializer
+    # permission_classes =
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    # permission_classes =
+
+    def get_title(self):
+        title_id = self.kwargs['title_id']
+        return get_object_or_404(Title, id=title_id)
+
+    def get_queryset(self):
+        title = self.get_title()
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = self.get_title()
+        serializer.save(author=self.request.user, title=title)
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+def generate_code():
+    list_numbers = [str(randint(0, 9)) for i in range(8)]
+    return ''.join(list_numbers)
+
+def  get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+def send_code(user):
+    confirmation_code = generate_code()
+    subject = 'Код подтверждения от Yamdb'
+    massage = f'{confirmation_code} - ваш код авторизации'
+    user_email = user.email
+    return send_mail(subject, massage, user_email)
+
+@api_view(['POST'])
+@permission_classes(['AllowAny'])
+def signup(request):
+    serializer = SignUpSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save
+        send_code(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes(['AllowAny'])
+def token(request):
+    serializer = TokenSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    user = get_object_or_404(User, username=serializer.data['username'])
+    refresh = RefreshToken.for_user(user)
+    
+    return Response({
+        'refresh': str(refresh),
+        'access': str(refresh.access_token)})
